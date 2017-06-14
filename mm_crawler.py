ae906ff3466e85b2fd2800e90afd67c0f08fdc6c
chenjiandongx <chenjiandongx@qq.com>
2017-06-14 22:56:19 +0800

    clean code

--- mm_crawler.py
+++ mm_crawler.py
@@ -18,7 +18,6 @@ def save_pic(pic_src, pic_cnt):
         with open(imgname, 'ab') as f:
             f.write(img.content)
             print(imgname)
-
     except Exception as e:
         print(e)
 
@@ -26,7 +25,6 @@ def save_pic(pic_src, pic_cnt):
 def make_dir(folder_name):
     """ 新建套图文件夹并切换到该目录下 """
     path = os.path.join(dir_path, folder_name)
-
     # 如果目录已经存在就不用再次爬取了，去重，提高效率。存在返回 False，否则反之
     if not os.path.exists(path):
         os.makedirs(path)
@@ -47,35 +45,30 @@ def delete_empty_dir(dir):
                 path = os.path.join(dir, d)     # 组装下一级地址
                 if os.path.isdir(path):
                     delete_empty_dir(path)      # 递归删除空文件夹
-
         if not os.listdir(dir):
             os.rmdir(dir)
             print("remove the empty dir: {}".format(dir))
     else:
         print("Please start your performance!") # 请开始你的表演
 
-
 lock = threading.Lock()     # 全局资源锁
 
 def urls_crawler(url):
     """ 爬虫入口，主要爬取操作 """
     try:
-        respone = requests.get(url, headers=headers, timeout=10).text
+        r = requests.get(url, headers=headers, timeout=10).text
         # 套图名，也作为文件夹名
-        folder_name = BeautifulSoup(respone, 'lxml').find('h2').text.encode('ISO-8859-1').decode('utf-8')
-
+        folder_name = BeautifulSoup(r, 'lxml').find('h2').text.encode('ISO-8859-1').decode('utf-8')
         with lock:
             if make_dir(folder_name):
                 # 套图张数
-                max_count = BeautifulSoup(respone, 'lxml').find('div', class_='page').find_all('a')[-2].get_text()
+                max_count = BeautifulSoup(r, 'lxml').find('div', class_='page').find_all('a')[-2].get_text()
                 # 套图页面
                 page_urls = [url + "/" + str(i) for i in range(1, int(max_count) + 1)]
                 # 图片地址
                 img_urls = []
-
                 for index, page_url in enumerate(page_urls):
                     result = requests.get(page_url, headers=headers, timeout=10).text
-
                     # 最后一张图片没有a标签直接就是img所以分开解析
                     if index + 1 < len(page_urls):
                         img_url = BeautifulSoup(result, 'lxml').find('div', class_='content').find('a').img['src']
@@ -86,13 +79,11 @@ def urls_crawler(url):
 
                 for cnt, url in enumerate(img_urls):
                     save_pic(url, cnt)
-
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-
     urls = ['http://mmjpg.com/mm/{cnt}'.format(cnt=cnt) for cnt in range(1, 953)]
     pool = Pool(processes=cpu_count())
     try:

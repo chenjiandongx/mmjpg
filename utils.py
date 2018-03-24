5c9bac7b5a878a770429c84687f51b7fbc049223
chenjiandongx <chenjiandongx@qq.com>
2018-03-24 22:36:52 +0800

    更新 json 文件

--- utils.py
+++ utils.py
@@ -2,16 +2,24 @@
 # coding=utf-8
 
 import os
+import json
+
+PATH = "https://raw.githubusercontent.com/chenjiandongx/mmjpg/master/yummy/{}"
 
 
 def file_name(file_dir):
     for root, dirs, files in os.walk(file_dir):
-        yield files
+        for file in files:
+            yield PATH.format(file)
 
 
-for index, file in enumerate(file_name("yummy")):
-    if index < 10:
-        print(file)
-    else:
-        break
+def gen_json(paths):
+    d = []
+    for path in paths:
+        d.append({"imgSrc": path})
+    with open("yummy.json", "w+") as f:
+        json.dump(d, f)
 
+
+if __name__ == "__main__":
+    gen_json(file_name('yummy'))
